e9c29bc68132b872da7ee34c9a9963c33a52de8b
Ashraf <ashrafmohamud@gmail.com>
2020-06-25 20:31:11 +0100

    hussain commit

---
+++ environment/provision.sh
@@ -0,0 +1,64 @@
+# sudo apt-get update -y
+# sudo apt-get install nginx -y
+# service nginx start
+# sudo apt-get install nodejs npm -y
+#
+#
+# npm install -g pm2
+#
+#
+#
+# # sudo apt install curl
+#
+# # curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+# #
+# # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+#
+# curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#
+# sudo apt-get install nodejs -y
+# # sudo apt-get install -y nodejs
+
+sudo apt-get update -y
+
+
+
+sudo apt-get install nginx -y
+
+
+
+apt-get install nodejs npm -y
+
+
+
+npm install -g pm2
+
+
+
+sudo nvm use 6
+
+
+
+service nginx start
+
+
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
+sudo apt-get install -y nodejs
+
+
+
+sudo npm install pm2 -
+
+
+
+#
+#
+# apt-get install -y g++
+# curl -sL https://deb.nodesource.com/setup_0.12 | sh
+# apt-get install -y nodejs
+# su vagrant
+# mkdir /home/vagrant/node_modules
+# cd /app
+# ln -s /home/vagrant/node_modules/ node_modules
+# npm install karma
